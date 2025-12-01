5576c1cc30d7600ad5c75549b0211e192a48d591
nuuuwan <nuuuwan@gmail.com>
2025-12-01 16:33:10 +0530

    feat: add location markers to MapView for enhanced visualization

--- src/view/orgas/MapView.js
+++ src/view/orgas/MapView.js
@@ -1,14 +1,18 @@
 import { MapContainer, TileLayer, CircleMarker, Popup } from "react-leaflet";
 import { useState, useEffect } from "react";
 import "leaflet/dist/leaflet.css";
-import { Station } from "../../nonview/core";
+import { Station, Location } from "../../nonview/core";
 
 export default function MapView() {
   const [stations, setStations] = useState([]);
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
     Station.listAll().then((loadedStations) => {
       setStations(loadedStations);
+    });
+    Location.listAll().then((loadedLocations) => {
+      setLocations(loadedLocations);
     });
   }, []);
 
@@ -22,12 +26,24 @@ export default function MapView() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {locations.map((location, index) => (
+        <CircleMarker
+          key={`location-${index}`}
+          center={location.latLng}
+          radius={6}
+          pathOptions={{ color: "none", fillColor: "gray", fillOpacity: 0.4 }}
+        >
+          <Popup>
+            <strong>{location.name}</strong>
+          </Popup>
+        </CircleMarker>
+      ))}
       {stations.map((station, index) => (
         <CircleMarker
-          key={index}
+          key={`station-${index}`}
           center={station.latLng}
           radius={8}
-          pathOptions={{ color: "red", fillColor: "red", fillOpacity: 0.5 }}
+          pathOptions={{ color: "none", fillColor: "red", fillOpacity: 0.5 }}
         >
           <Popup>
             <strong>{station.name}</strong>
