cd32167e5f5624f8f655e4b4ec4018045b2c90dd
nuuuwan <nuuuwan@gmail.com>
2025-12-02 22:04:34 +0530

    fix: refactor listAll method in Weather class for clarity and improved variable naming

--- src/nonview/core/Weather.js
+++ src/nonview/core/Weather.js
@@ -10,35 +10,35 @@ export default class Weather {
   }
 
   static async listAll() {
-    const rawDataList = await Cache.get("weather.listAll", async () => {
+    const latest = await Cache.get("weather.listAll", async () => {
       try {
         const response = await fetch(
-          "https://raw.githubusercontent.com/nuuuwan/weather_lk/refs/heads/data/list_all.json",
+          "https://raw.githubusercontent.com/nuuuwan/weather_lk/refs/heads/data/list_all.json"
         );
 
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const data = await response.json();
-        return data;
+        const rawDataList = await response.json();
+        const latest = rawDataList[rawDataList.length - 1];
+        return latest;
       } catch (error) {
         console.error("Error loading weather data:", error);
         return [];
       }
     });
-    const latest = rawDataList[rawDataList.length - 1];
     const weatherList = latest.weather_list;
     const timeUt = latest.date_ut + 8 * 3_600;
     return weatherList.map(
-      (weatherData) => new Weather({ ...weatherData, timeUt }),
+      (weatherData) => new Weather({ ...weatherData, timeUt })
     );
   }
 
   static async idx() {
     const list = await this.listAll();
     return Object.fromEntries(
-      list.map((weather) => [weather.locationName, weather]),
+      list.map((weather) => [weather.locationName, weather])
     );
   }
 
