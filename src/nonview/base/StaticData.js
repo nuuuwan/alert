60d234e977e915187108aa517beda30b37712812
nuuuwan <nuuuwan@gmail.com>
2025-12-02 10:39:44 +0530

    refactor: streamline data fetching in StaticData.listAll method

--- src/nonview/base/StaticData.js
+++ src/nonview/base/StaticData.js
@@ -5,23 +5,26 @@ export default class StaticData {
   }
 
   static async listAll() {
-    return await Cache.get(`${this.getClassID()}.listAll`, async () => {
-      try {
-        const response = await fetch(
-          process.env.PUBLIC_URL + `/data/static/${this.getClassID()}s.json`,
-        );
+    const rawDataList = await Cache.get(
+      `${this.getClassID()}.listAll`,
+      async () => {
+        try {
+          const response = await fetch(
+            process.env.PUBLIC_URL + `/data/static/${this.getClassID()}s.json`
+          );
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+
+          return await response.json();
+        } catch (error) {
+          console.error(`Error loading ${this.getClassID()}s:`, error);
+          return [];
         }
-
-        const data = await response.json();
-        return data.map((itemData) => new this(itemData));
-      } catch (error) {
-        console.error(`Error loading ${this.getClassID()}s:`, error);
-        return [];
       }
-    });
+    );
+    return rawDataList.map((rawData) => new this(rawData));
   }
 
   static async idx() {
