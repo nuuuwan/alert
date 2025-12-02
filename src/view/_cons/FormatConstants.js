6174b860ed9513b440d3ceebe7d9833ae280c338
nuuuwan <nuuuwan@gmail.com>
2025-12-02 15:29:32 +0530

    feat: update DATE_TIME_FORMAT to maintain year property in correct order

--- src/view/_cons/FormatConstants.js
+++ src/view/_cons/FormatConstants.js
@@ -2,9 +2,9 @@ export const DATE_TIME_FORMAT = {
   hour: "numeric",
   minute: "2-digit",
   hour12: true,
+  year: "numeric",
   month: "short",
   day: "numeric",
-  year: "numeric",
 };
 
 export const SHORT_DATE_TIME_FORMAT = {
