3abeb16da9afc92decfb3ac59b6c9df892ba6c38
nuuuwan <nuuuwan@gmail.com>
2025-12-02 13:21:46 +0530

    feat: refactor MapStationView to use StationDetails component for popup content

---
+++ src/view/moles/StationDetails.js
@@ -0,0 +1,55 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
+import WaterLevelChart from "./WaterLevelChart";
+
+export default function StationDetails({ station, stationToLatest }) {
+  const latestLevel = stationToLatest[station.name];
+  const alert = station.getAlert(latestLevel.waterLevelM);
+  const alertColor = alert.colorRgb;
+
+  const date = latestLevel.date;
+  const formattedDate = date.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  return (
+    <Box>
+      <Typography variant="overline" color="text.secondary" gutterBottom>
+        {station.riverName}
+      </Typography>
+      <Typography variant="h4" component="h1" gutterBottom>
+        {station.name}
+      </Typography>
+      <Typography variant="body1" component="h1" gutterBottom>
+        Gauging Station
+      </Typography>
+      <Typography variant="body2" color="text.secondary" gutterBottom>
+        {formattedDate}
+      </Typography>
+      <Typography variant="h3" component="div" sx={{ mt: 3, mb: 1 }}>
+        {latestLevel.waterLevelM.toFixed(2)}m
+      </Typography>
+
+      <Chip
+        label={alert.label}
+        sx={{
+          mt: 2,
+          backgroundColor: alertColor,
+          color: "white",
+          fontWeight: "bold",
+        }}
+      />
+
+      <Divider sx={{ my: 3 }} />
+
+      <WaterLevelChart station={station} />
+    </Box>
+  );
+}
