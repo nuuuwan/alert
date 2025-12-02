a453de9a9b6277b93a04c0b1fc13c48aaf4a82e1
nuuuwan <nuuuwan@gmail.com>
2025-12-02 08:36:49 +0530

    feat: add labels to location and station markers for improved visibility

--- src/view/moles/MapLocationView.js
+++ src/view/moles/MapLocationView.js
@@ -1,20 +1,38 @@
-import { CircleMarker, Popup } from "react-leaflet";
+import { CircleMarker, Popup, Marker } from "react-leaflet";
+import L from "leaflet";
 import { LOCATION_MARKER_RADIUS } from "../_cons/MapConstants";
 
 export default function MapLocationView({ locations }) {
   return (
     <>
       {locations.map((location, index) => (
-        <CircleMarker
-          key={`location-${index}`}
-          center={location.latLng}
-          radius={LOCATION_MARKER_RADIUS}
-          pathOptions={{ color: "none", fillColor: "gray", fillOpacity: 1 }}
-        >
-          <Popup>
-            <strong>{location.name}</strong>
-          </Popup>
-        </CircleMarker>
+        <>
+          <CircleMarker
+            key={`location-${index}`}
+            center={location.latLng}
+            radius={LOCATION_MARKER_RADIUS}
+            pathOptions={{ color: "none", fillColor: "gray", fillOpacity: 1 }}
+          >
+            <Popup>
+              <strong>{location.name}</strong>
+            </Popup>
+          </CircleMarker>
+          <Marker
+            key={`location-label-${index}`}
+            position={location.latLng}
+            icon={L.divIcon({
+              className: "location-label",
+              html: `<div style="font-size: ${
+                LOCATION_MARKER_RADIUS * 2
+              }px; color: #666; white-space: nowrap; margin-left: ${
+                LOCATION_MARKER_RADIUS * 4
+              }px;">${location.name}</div>`,
+              iconSize: [0, 0],
+              iconAnchor: [0, 0],
+            })}
+            interactive={false}
+          />
+        </>
       ))}
     </>
   );
--- src/view/moles/MapStationView.js
+++ src/view/moles/MapStationView.js
@@ -1,28 +1,50 @@
-import { CircleMarker, Popup } from "react-leaflet";
+import { CircleMarker, Popup, Marker } from "react-leaflet";
+import L from "leaflet";
 import { STATION_MARKER_RADIUS } from "../_cons/MapConstants";
 
 export default function MapStationView({ stations }) {
   return (
     <>
       {stations.map((station, index) => (
-        <CircleMarker
-          key={`station-${index}`}
-          center={station.latLng}
-          radius={STATION_MARKER_RADIUS}
-          pathOptions={{ color: "blue", fillColor: "white", fillOpacity: 1.0 }}
-        >
-          <Popup>
-            <strong>{station.name}</strong>
-            <br />
-            River: {station.riverName}
-            <br />
-            Alert Level: {station.alertLevelM}m
-            <br />
-            Minor Flood: {station.minorFloodLevelM}m
-            <br />
-            Major Flood: {station.majorFloodLevelM}m
-          </Popup>
-        </CircleMarker>
+        <>
+          <CircleMarker
+            key={`station-${index}`}
+            center={station.latLng}
+            radius={STATION_MARKER_RADIUS}
+            pathOptions={{
+              color: "blue",
+              fillColor: "white",
+              fillOpacity: 1.0,
+            }}
+          >
+            <Popup>
+              <strong>{station.name}</strong>
+              <br />
+              River: {station.riverName}
+              <br />
+              Alert Level: {station.alertLevelM}m
+              <br />
+              Minor Flood: {station.minorFloodLevelM}m
+              <br />
+              Major Flood: {station.majorFloodLevelM}m
+            </Popup>
+          </CircleMarker>
+          <Marker
+            key={`station-label-${index}`}
+            position={station.latLng}
+            icon={L.divIcon({
+              className: "station-label",
+              html: `<div style="font-size: ${
+                STATION_MARKER_RADIUS * 2
+              }px; color: #333; font-weight: 500; white-space: nowrap; margin-left: ${
+                STATION_MARKER_RADIUS * 3
+              }px;">${station.name}</div>`,
+              iconSize: [0, 0],
+              iconAnchor: [0, 0],
+            })}
+            interactive={false}
+          />
+        </>
       ))}
     </>
   );
