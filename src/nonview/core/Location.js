a92ba0ac75b3b94cf4b6be8ef142bae86652deb5
nuuuwan <nuuuwan@gmail.com>
2025-12-02 10:39:58 +0530

    feat: add getAlertLevel method to Location class

--- src/nonview/core/Location.js
+++ src/nonview/core/Location.js
@@ -1,5 +1,5 @@
 import { StaticData } from "../base";
-
+import Alert from "./Alert.js";
 export default class Location extends StaticData {
   static getClassID() {
     return "location";
@@ -7,5 +7,9 @@ export default class Location extends StaticData {
   constructor(data) {
     super(data);
     this.latLng = data.lat_lng;
+  }
+
+  getAlertLevel(waterLevelM) {
+    return Alert.NO_DATA;
   }
 }
