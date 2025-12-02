7508664531d0e29654a572188da5bea50c8e5547
nuuuwan <nuuuwan@gmail.com>
2025-12-02 12:41:10 +0530

    feat: enhance typography and update CSS for better font loading

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import "./App.css";
 import MapView from "./view/orgas/MapView";
 import Header from "./view/moles/Header";
 import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '"Ubuntu Sans"',
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+    ].join(","),
+  },
+});
 
 function App() {
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-      <Header />
-      <Box sx={{ flexGrow: 1, marginTop: "48px" }}>
-        <MapView />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+        <Header />
+        <Box sx={{ flexGrow: 1, marginTop: "48px" }}>
+          <MapView />
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
 
