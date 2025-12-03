12dc6f50fc814a264da0307ba9a63da09ee6a878
nuuuwan <nuuuwan@gmail.com>
2025-12-03 16:52:27 +0530

    feat: refactor DB class to streamline event data loading and improve structure

--- src/nonview/core/DB.js
+++ src/nonview/core/DB.js
@@ -7,44 +7,56 @@ import WeatherReport from "./events/WeatherReport";
 import GaugingStationPlaceRole from "./roles/GaugingStationPlaceRole";
 import DSD from "./ents/regions/admin_regions/DSD";
 export default class DB {
+  static getEventClasses() {
+    return [RiverWaterLevelMeasurement, WeatherReport, LandslideWarning];
+  }
+
+  static async getEventNameToEventListMap() {
+    const classList = this.getEventClasses();
+    const EventNameAndEventListList = await Promise.all(
+      classList.map(async (EventClass) => {
+        const eventList = await EventClass.listAll();
+        return [EventClass.name, eventList];
+      })
+    );
+    return Object.fromEntries(EventNameAndEventListList);
+  }
+
+  static async getIdToEventNameToEventListMap() {
+    const eventNameToEventListMap = await this.getEventNameToEventListMap();
+    const idToEventNameToEventListMap = {};
+    for (const [eventName, eventList] of Object.entries(
+      eventNameToEventListMap
+    )) {
+      eventList.forEach((event) => {
+        if (!idToEventNameToEventListMap[event.id]) {
+          idToEventNameToEventListMap[event.id] = {};
+        }
+        if (!idToEventNameToEventListMap[event.id][eventName]) {
+          idToEventNameToEventListMap[event.id][eventName] = [];
+        }
+        idToEventNameToEventListMap[event.id][eventName].push(event);
+      });
+    }
+    return idToEventNameToEventListMap;
+  }
+
   static async load() {
-    const riverWaterLevelMeasurements =
-      await RiverWaterLevelMeasurement.listAll();
-    const weatherReports = await WeatherReport.listAll();
-    const landslideWarnings = await LandslideWarning.listAll();
+    const idToEventNameToEventListMap =
+      await this.getIdToEventNameToEventListMap();
+    const placesIdx = await Place.idx();
+    const dsdIdx = await DSD.idx();
 
-    const activeGaugingStationPlaces =
-      await GaugingStationPlaceRole.listFromIds(
-        RiverWaterLevelMeasurement.uniqueIdsFromList(
-          riverWaterLevelMeasurements
-        )
-      );
-    const activeWeatherStationPlaces =
-      await WeatherStationPlaceRole.listFromIds(
-        WeatherReport.uniqueIdsFromList(weatherReports)
-      );
-    const activeLandslideRegions = await LandslideRegionRole.listFromIds(
-      LandslideWarning.uniqueIdsFromList(landslideWarnings)
-    );
-
-    const activePlaces = await Place.listFromIds([
-      ...GaugingStationPlaceRole.uniqueIdsFromList(activeGaugingStationPlaces),
-      ...WeatherStationPlaceRole.uniqueIdsFromList(activeWeatherStationPlaces),
-    ]);
-
-    const activeRegions = await DSD.listFromIds(
-      LandslideRegionRole.uniqueIdsFromList(activeLandslideRegions)
-    );
+    console.debug({
+      idToEventNameToEventListMap,
+      placesIdx,
+      dsdIdx,
+    });
 
     return {
-      riverWaterLevelMeasurements,
-      weatherReports,
-      landslideWarnings,
-      activeGaugingStationPlaces,
-      activeWeatherStationPlaces,
-      activeLandslideRegions,
-      activePlaces,
-      activeRegions,
+      idToEventNameToEventListMap,
+      placesIdx,
+      dsdIdx,
     };
   }
 }
