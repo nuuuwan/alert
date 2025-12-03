c685362d190a9c42056472104c32afb6cb8fd37b
nuuuwan <nuuuwan@gmail.com>
2025-12-03 15:04:30 +0530

    feat: add WeatherReport class with role class method and data handling

---
+++ src/nonview/core/events/WeatherReport.js
@@ -0,0 +1,26 @@
+import BaseEvent from "./BaseEvent.js";
+import WeatherStationPlaceRole from "../roles/WeatherStationPlaceRole.js";
+import DataWithIDMixin from "../../base/DataWithIDMixin.js";
+import DataWithTimeMixin from "../../base/DataWithTimeMixin.js";
+
+class WeatherReport extends BaseEvent {
+  static getRoleClass() {
+    return WeatherStationPlaceRole;
+  }
+
+  constructor(data) {
+    super({ id: data.id, timeUt: data.timeUt || data.time_ut });
+    this.rainMM = data.rainMM || data.rain_MM;
+    this.tempMinC = data.tempMinC || data.temp_min_c;
+    this.tempMaxC = data.tempMaxC || data.temp_max_c;
+  }
+
+  static getUrl() {
+    return "https://raw.githubusercontent.com/nuuuwan/weather_lk/refs/heads/data/latest_flat.json";
+  }
+}
+
+Object.assign(WeatherReport.prototype, DataWithIDMixin);
+Object.assign(WeatherReport.prototype, DataWithTimeMixin);
+
+export default WeatherReport;
