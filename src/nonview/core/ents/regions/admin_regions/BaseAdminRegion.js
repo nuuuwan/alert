83480581ec74afe8f8865067edb4b5e5da4c07e6
nuuuwan <nuuuwan@gmail.com>
2025-12-03 11:08:29 +0530

    feat: implement fetchTSV method in BaseAdminRegion for TSV data retrieval

--- src/nonview/core/ents/regions/admin_regions/BaseAdminRegion.js
+++ src/nonview/core/ents/regions/admin_regions/BaseAdminRegion.js
@@ -6,11 +6,42 @@ export default class BaseAdminRegion extends BaseRegion {
     throw new Error("Not Implemented");
   }
 
+  constructor(id, name, population2012) {
+    super(id, name);
+    this.population2012 = population2012;
+  }
+
   async getLngLatListList() {
     return await WWW.fetchJSON(
       "https://raw.githubusercontent.com" +
         "/nuuuwan/gig-data/refs/heads/master" +
-        `/geo/${this.constructor.getAdminRegionType()}/${this.id}.json`,
+        `/geo/${this.constructor.getAdminRegionType()}/${this.id}.json`
     );
+  }
+
+  static async listAll() {
+    const url =
+      "https://raw.githubusercontent.com" +
+      "/nuuuwan/gig-data/refs/heads/master" +
+      `/ents/${this.constructor.getAdminRegionType()}.tsv`;
+    const dList = await WWW.fetchTSV(url);
+    return dList.map(
+      (d) =>
+        new this(
+          d.id,
+          d.name,
+          d.population_2012 ? parseInt(d.population_2012) : null
+        )
+    );
+  }
+
+  static async idx() {
+    const list = await this.listAll();
+    return Object.fromEntries(list.map((region) => [region.id, region]));
+  }
+
+  static async fromID(id) {
+    const index = await this.idx();
+    return index[id] || null;
   }
 }
