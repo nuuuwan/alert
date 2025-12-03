b4520d9c5a6618e4d53a87a16c03b984af95f8ea
nuuuwan <nuuuwan@gmail.com>
2025-12-03 15:19:34 +0530

    feat: refactor LandslideWarning class by removing unused properties and methods

--- src/nonview/core/events/LandslideWarning.js
+++ src/nonview/core/events/LandslideWarning.js
@@ -7,30 +7,14 @@ export default class LandslideWarning extends BaseEvent {
     return LandslideRegionRole;
   }
   constructor(data) {
-    this.dateID = data.dateID || data.date_id;
-    this.levelToDistrictToDSD =
-      data.levelToDistrictToDSD || data.level_to_district_to_dsds || {};
+    super({
+      id: data.id,
+      timeUt: data.timeUt || data.time_ut,
+    });
+    this.threatLevel = data.threatLevel || data.threat_level;
   }
 
-  static async getLatest() {
-    const cachedData = await Cache.get("landslideWarning.listAll", async () => {
-      try {
-        const response = await fetch(
-          "https://raw.githubusercontent.com/nuuuwan/lk_dmc_landslides/refs/heads/main/data/latest.json",
-        );
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error("Error loading landslide warnings:", error);
-        return [];
-      }
-    });
-
-    return new LandslideWarning(cachedData);
+  static getUrl() {
+    return "https://raw.githubusercontent.com/nuuuwan/lk_dmc_landslides/refs/heads/main/data/latest_flat.json";
   }
 }
