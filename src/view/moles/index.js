3021a07a9d86022758f7bc99789cc86d4baf2737
nuuuwan <nuuuwan@gmail.com>
2025-12-04 18:28:28 +0530

    updated indices

--- src/view/moles/index.js
+++ src/view/moles/index.js
@@ -5,7 +5,6 @@ import CustomDrawer from "./CustomDrawer";
 import DetailsHeader from "./DetailsHeader";
 import DownloadableContent from "./DownloadableContent";
 import EntDetails from "./EntDetails";
-import MapEntView from "./MapEntView";
 import MapPlaceView from "./MapPlaceView";
 import MapRegionView from "./MapRegionView";
 
@@ -16,7 +15,6 @@ export {
   DetailsHeader,
   DownloadableContent,
   EntDetails,
-  MapEntView,
   MapPlaceView,
   MapRegionView,
 };
