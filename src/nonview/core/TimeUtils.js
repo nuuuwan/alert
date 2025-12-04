f540a4e30e8b2d37b3ed6b23d2789aac75b41f8c
nuuuwan <nuuuwan@gmail.com>
2025-12-04 14:27:28 +0530

    refactor: adjust parse methods in TimeUtils to account for Sri Lanka timezone offset

--- src/nonview/core/TimeUtils.js
+++ src/nonview/core/TimeUtils.js
@@ -1,9 +1,13 @@
 export default class TimeUtils {
+  // Sri Lanka timezone offset: UTC+5:30 (19800 seconds)
+  static SRI_LANKA_OFFSET_SECONDS = 5.5 * 3600;
+
   static parseYYYYMMDD(datePart) {
     const year = Number(datePart.slice(0, 4));
     const month = Number(datePart.slice(4, 6)) - 1;
     const day = Number(datePart.slice(6, 8));
-    return Date.UTC(year, month, day) / 1000;
+    // Convert from Sri Lanka time to UTC by subtracting the offset
+    return Date.UTC(year, month, day) / 1000 - this.SRI_LANKA_OFFSET_SECONDS;
   }
 
   static parseYYYYMMDDHHHMMSS(dateTimePart) {
@@ -13,6 +17,10 @@ export default class TimeUtils {
     const hour = Number(dateTimePart.slice(8, 10));
     const minute = Number(dateTimePart.slice(10, 12));
     const second = Number(dateTimePart.slice(12, 14));
-    return Date.UTC(year, month, day, hour, minute, second) / 1000;
+    // Convert from Sri Lanka time to UTC by subtracting the offset
+    return (
+      Date.UTC(year, month, day, hour, minute, second) / 1000 -
+      this.SRI_LANKA_OFFSET_SECONDS
+    );
   }
 }
