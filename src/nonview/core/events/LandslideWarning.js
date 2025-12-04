d48eaba9a97005ed693b7efb08ab8ad60d50c21e
nuuuwan <nuuuwan@gmail.com>
2025-12-04 13:38:42 +0530

    feat: update LandslideWarning to use alert_data.json and integrate TimeUtils for date parsing

--- src/nonview/core/events/LandslideWarning.js
+++ src/nonview/core/events/LandslideWarning.js
@@ -1,7 +1,7 @@
 import LandslideArea from "../roles/LandslideArea.js";
 import BaseEvent from "./BaseEvent.js";
 import LandslideThreatLevel from "./LandslideThreatLevel.js";
-
+import TimeUtils from "../TimeUtils.js";
 export default class LandslideWarning extends BaseEvent {
   static getRoleClass() {
     return LandslideArea;
@@ -11,7 +11,32 @@ export default class LandslideWarning extends BaseEvent {
   }
 
   static getUrl() {
-    return "https://raw.githubusercontent.com/nuuuwan/lk_dmc_landslides/refs/heads/main/data/latest_flat.json";
+    return "https://raw.githubusercontent.com/nuuuwan/lk_dmc_landslides/refs/heads/main/data/alert_data.json";
+  }
+
+  static rawDataToRawDataList(rawData) {
+    const minTimeUt = Math.floor(Date.now() / 1000) - 2 * 24 * 3600;
+    return Object.entries(rawData["event_data"]).reduce(function (
+      rawDataList,
+      [id, datePartToLevel]
+    ) {
+      return Object.entries(datePartToLevel).reduce(function (
+        rawDataList,
+        [datePart, level]
+      ) {
+        const timeUt = TimeUtils.parseYYYYMMDD(datePart);
+        if (timeUt >= minTimeUt) {
+          rawDataList.push({
+            id: id,
+            time_ut: timeUt,
+            threat_level: level,
+          });
+        }
+        return rawDataList;
+      },
+      rawDataList);
+    },
+    []);
   }
 
   constructor(data) {
