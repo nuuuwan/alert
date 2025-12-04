2959c3c8cf9fe5f2431f7c4d86025b24748fe7b5
nuuuwan <nuuuwan@gmail.com>
2025-12-04 06:57:51 +0530

    refactor: reorder priority and color getters in BaseEvent class

--- src/nonview/core/events/BaseEvent.js
+++ src/nonview/core/events/BaseEvent.js
@@ -9,13 +9,16 @@ class BaseEvent {
     throw new Error("Not implemented");
   }
 
-  get priority() {
-    return 0;
-  }
-
   constructor({ id, timeUt }) {
     this.id = id; // id of the Ent
     this.timeUt = parseInt(timeUt);
+  }
+
+  get priority() {
+    return 0;
+  }
+  get color() {
+    return "cyan";
   }
 }
 
