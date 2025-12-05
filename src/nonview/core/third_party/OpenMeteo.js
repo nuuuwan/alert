a9be90af11dd9fe402d9a64e23654f1e950db4b3
nuuuwan <nuuuwan@gmail.com>
2025-12-05 19:35:17 +0530

    feat: implement OpenMeteo class for weather data retrieval

---
+++ src/nonview/core/third_party/OpenMeteo.js
@@ -0,0 +1,16 @@
+export default class OpenMeteo {
+  static async getData({ latLng }) {
+    const params = {
+      latitude: latLng[0],
+      longitude: latLng[1],
+      hourly: "temperature_2m",
+      timezone: "Asia/Colombo",
+    };
+    const url = "https://api.open-meteo.com/v1/forecast";
+    const responses = await fetchWeatherApi(url, params);
+    const response = responses[0];
+    return {
+      elevation: response.elevation(),
+    };
+  }
+}
