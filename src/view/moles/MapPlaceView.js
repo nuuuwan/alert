84ce0105c6d981cf6f4300d274f01de9b1a57378
nuuuwan <nuuuwan@gmail.com>
2025-12-06 20:49:53 +0530

    refactor: Replace RiverStationIcon with EntIcon in MapPlaceView for improved icon handling

--- src/view/moles/MapPlaceView.js
+++ src/view/moles/MapPlaceView.js
@@ -1,14 +1,14 @@
 import { Marker } from "react-leaflet";
 import L from "leaflet";
 import { LOCATION_MARKER_RADIUS } from "../_cons/MapConstants";
-import RiverStationIcon from "../atoms/icons/RiverStationIcon";
+import ReactDOMServer from "react-dom/server";
+import EntIcon from "../atoms/EntIcon";
 
 export default function MapPlaceView({ place, onClick }) {
   if (!place) {
     throw new Error("MapPlaceView requires a place prop");
   }
 
-  const Icon = RiverStationIcon;
   const iconSize = LOCATION_MARKER_RADIUS * 8;
   const circleSize = iconSize * 1.1;
   const opacity = 0.5;
@@ -19,6 +19,15 @@ export default function MapPlaceView({ place, onClick }) {
     onClick(place, e);
   };
 
+  const entIconSvg = ReactDOMServer.renderToStaticMarkup(
+    <EntIcon
+      ent={place}
+      size={iconSize}
+      color={placeColor}
+      strokeColor={placeColor}
+    />
+  );
+
   return (
     <Marker
       position={place.latLng}
@@ -28,11 +37,7 @@ export default function MapPlaceView({ place, onClick }) {
             <div style="position: relative; width: ${iconSize}px; height: ${iconSize}px; display: flex; align-items: cplaceer; justify-contplace: cplaceer; opacity: ${opacity};">
               <div style="position: absolute; width: ${circleSize}px; height: ${circleSize}px; background-color: white; border-radius: 50%; box-shadow: 0 2px 4px rgba(0,0,0,0.2);"></div>
               <div style="position: relative; z-index: 1;">
-                ${Icon({
-                  size: iconSize,
-                  color: placeColor,
-                  strokeColor: placeColor,
-                })}
+                ${entIconSvg}
               </div>
             </div>
           `,
