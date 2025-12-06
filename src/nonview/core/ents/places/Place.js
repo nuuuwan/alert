abb265dc31a098e97b1b754b0ca8f1814603eafa
nuuuwan <nuuuwan@gmail.com>
2025-12-06 08:31:16 +0530

    Updated Place format - LatLng only

--- src/nonview/core/ents/places/Place.js
+++ src/nonview/core/ents/places/Place.js
@@ -1,19 +1,7 @@
-import DataWithIDMixin from "../../base/mixins/DataWithIDMixin.js";
 class Place {
-  static getEntTypeTitle() {
-    return "Place";
-  }
-  constructor(data) {
-    this.id = data.id;
-    this.name = data.name;
-    this.latLng = data.latLng || data.lat_lng;
-  }
-
-  static getUrl() {
-    return process.env.PUBLIC_URL + `/data/static/places.json`;
+  constructor({ latLng }) {
+    this.latLng = latLng;
   }
 }
-
-Object.assign(Place, DataWithIDMixin);
 
 export default Place;
