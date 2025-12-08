15cb8bb7afab1dac1d01acc5a1772f452eec46e5
nuuuwan <nuuuwan@gmail.com>
2025-12-08 12:54:18 +0530

    fix: Implement loadCentroidPlace method in Region class and update DSDDetails component to handle centroid place loading

--- src/nonview/core/ents/regions/Region.js
+++ src/nonview/core/ents/regions/Region.js
@@ -1,8 +1,15 @@
+import Place from "../../ents/places/Place.js";
 export default class Region {
   static getEntTypeName() {
     return "Region";
   }
   constructor({ multiPolygon }) {
     this.multiPolygon = multiPolygon;
+  }
+
+  async loadCentroidPlace() {
+    const centroidLatLng = this.multiPolygon.getCentroid();
+    const centroidPlace = await Place.load({ latLng: centroidLatLng });
+    return centroidPlace;
   }
 }
--- src/view/orgas/DSDDetails.js
+++ src/view/orgas/DSDDetails.js
@@ -1,15 +1,36 @@
 import LandslideWarningView from "../atoms/LandslideWarningView";
 import DSD from "../../nonview/core/ents/regions/admin_regions/DSD";
 import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
+import PlaceDetails from "../orgas/PlaceDetails";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 
 export default function DSDDetails({ region }) {
-  if (!(region instanceof DSD)) return null;
+  if (!(region instanceof DSD)) {
+    throw new Error("region is not a DSD");
+  }
+
+  const [centroidPlace, setCentroidPlace] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const place = await region.loadCentroidPlace();
+      await place.loadDetails();
+      setCentroidPlace(place);
+    })();
+  }, [region]);
+
   return (
     <Box>
       <LandslideWarningView
         level={region.latestLandslideWarningLevel}
         timeUt={region.latestLandslideWarningTimeUt}
       />
+      <Divider sx={{ my: 2 }} />
+      <Typography variant="caption" display="block" gutterBottom>
+        The details below are rendered for the centroid of the region.
+      </Typography>
+      {centroidPlace && <PlaceDetails place={centroidPlace} />}
     </Box>
   );
 }
