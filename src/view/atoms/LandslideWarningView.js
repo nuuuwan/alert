6618aaeeff324f2ce89243b99e5410d66c396d7b
nuuuwan <nuuuwan@gmail.com>
2025-12-08 13:53:14 +0530

    fix: Ensure level is not null in LandslideWarningView and update unit assignment for clarity

--- src/view/atoms/LandslideWarningView.js
+++ src/view/atoms/LandslideWarningView.js
@@ -4,19 +4,18 @@ import WarningIcon from "@mui/icons-material/Warning";
 import { getAlertColor } from "../_cons/StyleConstants";
 
 export default function LandslideWarningView({ level, timeUt }) {
-  if (level == null) return null;
+  if (level === null) {
+    throw new Error("level is null");
+  }
 
   let value = "No Warning";
-  let unit = "";
+  let unit = `Level ${level}`;
   if (level === 1) {
     value = "Watch";
-    unit = "Level 1";
   } else if (level === 2) {
     value = "Alert";
-    unit = "Level 2";
   } else if (level === 3) {
     value = "Evacuate";
-    unit = "Level 3";
   }
   const color = getAlertColor(level);
 
