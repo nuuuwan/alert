7a0ce3b52185eb422188077491b2ea882f2ccef9
nuuuwan <nuuuwan@gmail.com>
2025-12-08 15:08:25 +0530

    refactor: Enhance DSD class with sex/age data loading and improve landslide warning handling

--- src/nonview/core/ents/regions/admin_regions/DSD.js
+++ src/nonview/core/ents/regions/admin_regions/DSD.js
@@ -20,22 +20,24 @@ export default class DSD extends AdminRegion {
       const dsdIDToDateStrToLevel = alertData["event_data"];
       const maxDateStr = Object.values(dsdIDToDateStrToLevel).reduce(function (
         maxDateStr,
-        dateStrToLevel,
+        dateStrToLevel
       ) {
         return Object.keys(dateStrToLevel).reduce(function (
           maxDateStr,
-          dateStr,
+          dateStr
         ) {
           return dateStr > maxDateStr ? dateStr : maxDateStr;
-        }, maxDateStr);
-      }, "00000000");
+        },
+        maxDateStr);
+      },
+      "00000000");
       const dsdIDToLatestLandslideWarning = Object.fromEntries(
         Object.entries(dsdIDToDateStrToLevel)
           .map(([dsdID, dateStrToLevel]) => [
             dsdID,
             dateStrToLevel[maxDateStr] || 0,
           ])
-          .filter((entry) => entry[1]),
+          .filter((entry) => entry[1])
       );
       const timeUt = TimeUtils.parseYYYYMMDD(maxDateStr) + 3_600 * 16;
       return {
@@ -46,8 +48,14 @@ export default class DSD extends AdminRegion {
   }
 
   async loadDetails() {
+    await Promise.all([this.loadLandslideWarningData(), this.loadSexAgeData()]);
+    return this;
+  }
+
+  async loadLandslideWarningData() {
     const { timeUt, dsdIDToLatestLandslideWarning } =
       await DSD.loadAllWarningData();
+
     this.latestLandslideWarningLevel =
       dsdIDToLatestLandslideWarning[this.id] || 0;
     this.latestLandslideWarningTimeUt = timeUt;
@@ -62,5 +70,38 @@ export default class DSD extends AdminRegion {
     const warningsData = await DSD.loadAllWarningData();
     const dsdIds = Object.keys(warningsData.dsdIDToLatestLandslideWarning);
     return await DSD.loadFromIds(dsdIds);
+  }
+
+  static async loadAllSexAgeDataIdx() {
+    const url =
+      "https://raw.githubusercontent.com/nuuuwan/lk_census_2024/refs/heads" +
+      "/main/data/Basic-Population" +
+      "/Population-by-sex-age-and-district-according-to-DSD/data.json";
+
+    const dList = await Cache.get("DSD.loadAllSexAgeData", async () => {
+      return await WWW.fetch(url);
+    });
+    const idx = Object.fromEntries(dList.map((d) => [d["region_id"], d]));
+    console.debug(idx);
+    return idx;
+  }
+
+  async loadSexAgeData() {
+    const idx = await DSD.loadAllSexAgeDataIdx();
+    const data = idx[this.id];
+    if (!data) {
+      console.error(`No sex/age data found for DSD id ${this.id}`);
+      return this;
+    }
+    this.sexAgeData = {
+      population: data["total"],
+      malePopulation: data["sex-male"],
+      femalePopulation: data["sex-female"],
+      ageUnder15: data["age-under-15"],
+      age15to59: data["age-15-to-59"],
+      age60to64: data["age-60-to-64"],
+      age65andOver: data["age-65-and-over"],
+    };
+    return this;
   }
 }
