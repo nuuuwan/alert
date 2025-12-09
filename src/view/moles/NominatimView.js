157d43e7a211680420d01e817ce02d3115a2a9b9
nuuuwan <nuuuwan@gmail.com>
2025-12-09 16:25:39 +0530

    refactor: Simplify NominatimView component by removing unused code and improving loading state handling

--- src/view/moles/NominatimView.js
+++ src/view/moles/NominatimView.js
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import Nominatim from "../../nonview/core/third_party/Nominatim";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import AdminRegionView from "../atoms/AdminRegionView";
-import District from "../../nonview/core/ents/regions/admin_regions/District";
-import Province from "../../nonview/core/ents/regions/admin_regions/Province";
+import CircularProgress from "@mui/material/CircularProgress";
+import { COLORS } from "../_cons/StyleConstants";
+import MapIcon from "@mui/icons-material/Map";
+import IconButton from "@mui/material/IconButton";
 
 function NominatimView({ latlng }) {
   const [data, setData] = useState(null);
@@ -27,37 +28,15 @@ function NominatimView({ latlng }) {
     return <div>Error: {error.message}</div>;
   }
 
-  if (!data || !data.address) {
-    return <div>Loading...</div>;
+  if (!data) {
+    return <CircularProgress />;
   }
 
-  const address = data.address;
-  const districtId = address["ISO3166-2-lvl5"];
-  const provinceId = address["ISO3166-2-lvl4"];
-
-  let displayNameShorter = data.display_name;
-  displayNameShorter = displayNameShorter.replace(
-    `, ${data.address.country}`,
-    ""
-  );
-  displayNameShorter = displayNameShorter.replace(
-    `, ${data.address.state}`,
-    ""
-  );
-  displayNameShorter = displayNameShorter.replace(
-    `, ${data.address.state_district}`,
-    ""
-  );
-
   return (
-    <Box>
-      <Typography variant="caption" color="textSecondary">
-        {displayNameShorter}
+    <Box display="flex" alignItems="center">
+      <Typography variant="caption" color={COLORS.neutralLight}>
+        {data.display_name}
       </Typography>
-      <Box>
-        <AdminRegionView AdminRegionClass={District} id={districtId} />
-        <AdminRegionView AdminRegionClass={Province} id={provinceId} />
-      </Box>
     </Box>
   );
 }
