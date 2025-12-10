6152915435f9c9002aeb829627d3b0b44629f4c6
nuuuwan <nuuuwan@gmail.com>
2025-12-10 14:28:31 +0530

    refactor: Move getTestData method to Nominatim class and update reverseGeocode to use it for test mode

--- src/nonview/core/third_party/Nominatim.js
+++ src/nonview/core/third_party/Nominatim.js
@@ -1,7 +1,16 @@
 import WWW from "../../base/WWW";
 
 export default class Nominatim {
-  static async reverseGeocode(latLng) {
+  static getTestData() {
+    return {
+      display_name: "Test Place, Test City",
+    };
+  }
+
+  static async reverseGeocode(latLng, isTest = true) {
+    if (isTest) {
+      return this.getTestData();
+    }
     const [lat, lon] = latLng.raw();
     const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`;
     try {
