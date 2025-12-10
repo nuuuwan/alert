a48ee4aa7c408765fb9fb2f34c17769e98e417a4
nuuuwan <nuuuwan@gmail.com>
2025-12-10 15:01:23 +0530

    refactor: Remove unused state and effect hooks from DSDDetails component

--- src/view/orgas/DSDDetails.js
+++ src/view/orgas/DSDDetails.js
@@ -1,23 +1,12 @@
 import LandslideWarningView from "../atoms/LandslideWarningView";
 import DSD from "../../nonview/core/ents/regions/admin_regions/DSD";
 import Box from "@mui/material/Box";
-import { useEffect, useState } from "react";
 import SexAgeView from "../moles/SexAgeView";
-import NearbyPlacesView from "../moles/NearbyPlacesView";
 
 export default function DSDDetails({ region }) {
   if (!(region instanceof DSD)) {
     throw new Error("region is not a DSD");
   }
-
-  const [centroidPlace, setCentroidPlace] = useState(null);
-  useEffect(() => {
-    (async () => {
-      const place = await region.loadCentroidPlace();
-      await place.loadDetails();
-      setCentroidPlace(place);
-    })();
-  }, [region]);
 
   return (
     <Box>
