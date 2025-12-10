c86f92c9491ec7bf6989443613d0321c791ca91a
nuuuwan <nuuuwan@gmail.com>
2025-12-10 08:54:09 +0530

    feat: Enhance MapViewInner to load details for City and update selected entity logic

--- src/view/orgas/MapViewInner.js
+++ src/view/orgas/MapViewInner.js
@@ -8,6 +8,8 @@ import DSD from "../../nonview/core/ents/regions/admin_regions/DSD";
 import GeoLocation from "../../nonview/base/GeoLocation";
 import Box from "@mui/material/Box";
 import Place from "../../nonview/core/ents/places/Place";
+import City from "../../nonview/core/ents/places/City";
+import LatLng from "../../nonview/base/geos/LatLng";
 
 export default function MapViewInner({
   centerLatLng,
@@ -25,6 +27,8 @@ export default function MapViewInner({
   const [dsdEnts, setDsdEnts] = useState([]);
   const [browserLatLng, setBrowserLatLng] = useState(null);
 
+  // Default Multiple Ent Loading
+
   useEffect(() => {
     async function fetch() {
       const HydrometricStations = await HydrometricStation.loadWithAlerts();
@@ -41,6 +45,63 @@ export default function MapViewInner({
     }
     fetchDsdEnts();
   }, []);
+
+  // Single Ent Loading
+
+  useEffect(() => {
+    async function fetchSelectedDsd() {
+      if (dsdNameId) {
+        const dsd = await DSD.loadFromName(dsdNameId);
+        if (dsd) {
+          await dsd.loadDetails();
+          setSelectedEnt(dsd);
+        }
+      }
+    }
+    fetchSelectedDsd();
+  }, [dsdNameId]);
+
+  useEffect(() => {
+    async function fetchHydrometricStation() {
+      if (hydrometricStationNameId) {
+        const hydrometricStation = await HydrometricStation.loadFromName(
+          hydrometricStationNameId
+        );
+        if (hydrometricStation) {
+          await hydrometricStation.loadDetails();
+          setSelectedEnt(hydrometricStation);
+        }
+      }
+    }
+    fetchHydrometricStation();
+  }, [hydrometricStationNameId]);
+
+  useEffect(() => {
+    async function fetchCity() {
+      if (cityNameId) {
+        const city = await City.loadFromName(cityNameId);
+        if (city) {
+          await city.loadDetails();
+          setSelectedEnt(city);
+        }
+      }
+    }
+    fetchCity();
+  }, [cityNameId]);
+
+  useEffect(() => {
+    async function fetchPlace() {
+      if (placeLatLngId) {
+        const latLng = LatLng.fromId(placeLatLngId);
+        const place = await Place.load({ latLng });
+        if (place) {
+          await place.loadDetails();
+          setSelectedEnt(place);
+        }
+      }
+    }
+    fetchPlace();
+  }, [placeLatLngId]);
 
   useEffect(() => {
     async function fetchBrowserLocation() {
