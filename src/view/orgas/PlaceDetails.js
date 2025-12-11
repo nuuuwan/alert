64536fb9a3da67386fc8e08404ff0d0e7ddfd4fc
nuuuwan <nuuuwan@gmail.com>
2025-12-11 10:50:46 +0530

    refactor: Remove Grid layout from PlaceDetails for simplified structure

--- src/view/orgas/PlaceDetails.js
+++ src/view/orgas/PlaceDetails.js
@@ -1,5 +1,4 @@
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
 import SatelliteImageView from "../atoms/SatelliteImageView";
 import OpenMeteoView from "../moles/OpenMeteoView";
 import HydrometricStationDetails from "../moles/HydrometricStationDetails";
@@ -10,20 +9,13 @@ import NaturalDisasterRisk from "../moles/NaturalDisasterRisk";
 export default function PlaceDetails({ place }) {
   return (
     <Box sx={{ p: 1, m: 1, mb: 10 }}>
-      <Grid container spacing={2}>
-        <Grid size={{ xs: 12, md: 6 }}>
-          {place instanceof HydrometricStation && (
-            <HydrometricStationDetails place={place} />
-          )}
-          <NaturalDisasterRisk place={place} />
-          <OpenElevationView place={place} />
-          <OpenMeteoView place={place} />
-        </Grid>
-
-        <Grid size={{ xs: 12, md: 6 }}>
-          <SatelliteImageView place={place} />
-        </Grid>
-      </Grid>
+      {place instanceof HydrometricStation && (
+        <HydrometricStationDetails place={place} />
+      )}
+      <NaturalDisasterRisk place={place} />
+      <OpenElevationView place={place} />
+      <OpenMeteoView place={place} />
+      <SatelliteImageView place={place} />
     </Box>
   );
 }
