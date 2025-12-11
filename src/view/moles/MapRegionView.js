df7e7a1529cd2e83d2c5ac9c6a0bcc60e62fa252
nuuuwan <nuuuwan@gmail.com>
2025-12-11 17:21:37 +0530

    refactor: Remove unused imports and navigate hook from MapRegionView

--- src/view/moles/MapRegionView.js
+++ src/view/moles/MapRegionView.js
@@ -1,11 +1,8 @@
 import { Polygon } from "react-leaflet";
-import L from "leaflet";
 import DSD from "../../nonview/core/ents/regions/admin_regions/DSD";
 import { COLORS, getAlertColor } from "../_cons/StyleConstants";
-import { useNavigate } from "react-router-dom";
 
 export default function MapRegionView({ region }) {
-  const navigate = useNavigate();
   let color = COLORS.neutral;
   if (region instanceof DSD) {
     const dsd = region;
