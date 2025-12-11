0eb751c55e1703a7624659d72f1863d1b1ae1664
nuuuwan <nuuuwan@gmail.com>
2025-12-11 10:31:24 +0530

    refactor: Simplify CircularProgress positioning in SatelliteImageView

--- src/view/atoms/SatelliteImageView.js
+++ src/view/atoms/SatelliteImageView.js
@@ -49,14 +49,7 @@ export default function SatelliteImageView({ place }) {
             position: "relative",
           }}
         >
-          <CircularProgress
-            sx={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-            }}
-          />
+          <CircularProgress />
         </Box>
       )}
       <Box
