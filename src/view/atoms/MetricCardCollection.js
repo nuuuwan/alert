4da32a3387de26db96b3743d92ca9924d524f6ba
nuuuwan <nuuuwan@gmail.com>
2025-12-11 09:56:57 +0530

    feat: Refactor MetricCardCollection to use Accordion for better UI organization

--- src/view/atoms/MetricCardCollection.js
+++ src/view/atoms/MetricCardCollection.js
@@ -1,23 +1,29 @@
 import Box from "@mui/material/Box";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import SourceView from "./SourceView";
 
 export default function MetricCardCollection({ title, children, sourceList }) {
   return (
-    <Box>
-      <Typography variant="body2" gutterBottom>
-        {title}
-      </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          gap: 1,
-          flexWrap: "wrap",
-        }}
-      >
-        {children}
-      </Box>
-      <SourceView sourceList={sourceList} />
-    </Box>
+    <Accordion defaultExpanded={false}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography variant="body2">{title}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 1,
+            flexWrap: "wrap",
+          }}
+        >
+          {children}
+        </Box>
+        <SourceView sourceList={sourceList} />
+      </AccordionDetails>
+    </Accordion>
   );
 }
