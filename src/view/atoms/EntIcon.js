47792fcd8ff725fff437ecf19a8438ac4c99a1ad
nuuuwan <nuuuwan@gmail.com>
2025-12-12 12:48:16 +0530

    Remove RoleIcon and WeatherStationIcon components to streamline icon management

--- src/view/atoms/EntIcon.js
+++ src/view/atoms/EntIcon.js
@@ -1,27 +1,31 @@
 import Place from "../../nonview/core/ents/places/Place";
 import HydrometricStation from "../../nonview/core/ents/places/HydrometricStation";
-import AdminRegion from "../../nonview/core/ents/regions/admin_regions/AdminRegion";
-import HydrometricStationIcon from "./icons/HydrometricStationIcon";
-import AdminRegionIcon from "./icons/AdminRegionIcon";
 import LocationCityIcon from "@mui/icons-material/LocationCity";
 import { COLORS, getAlertColor } from "../_cons/StyleConstants";
 import City from "../../nonview/core/ents/places/City";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import WaterIcon from "@mui/icons-material/Water";
+import MapIcon from "@mui/icons-material/Map";
+import AdminRegion from "../../nonview/core/ents/regions/admin_regions/AdminRegion";
+
+function getIconClass(ent) {
+  if (ent instanceof HydrometricStation) {
+    return WaterIcon;
+  }
+  if (ent instanceof City) {
+    return LocationCityIcon;
+  }
+  if (ent instanceof Place) {
+    return LocationOnIcon;
+  }
+  if (ent instanceof AdminRegion) {
+    return MapIcon;
+  }
+  return LocationOnIcon;
+}
 
 export default function EntIcon({ ent, color, size }) {
   color = color || getAlertColor(ent.alertLevel) || COLORS.neutral;
-
-  if (ent instanceof HydrometricStation) {
-    return <HydrometricStationIcon color={color} size={size} />;
-  }
-  if (ent instanceof City) {
-    return <LocationCityIcon style={{ color }} sx={{ fontSize: size || 48 }} />;
-  }
-  if (ent instanceof Place) {
-    return <LocationOnIcon style={{ color }} sx={{ fontSize: size || 48 }} />;
-  }
-  if (ent instanceof AdminRegion) {
-    return <AdminRegionIcon color={color} size={size} />;
-  }
-  return <LocationOnIcon style={{ color }} sx={{ fontSize: size || 48 }} />;
+  const IconClass = getIconClass(ent);
+  return <IconClass sx={{ color, fontSize: size || 48 }} />;
 }
