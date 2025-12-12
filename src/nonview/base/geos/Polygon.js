c41f7873956a7e57e079ca485d173c392ba0009a
nuuuwan <nuuuwan@gmail.com>
2025-12-12 16:44:55 +0530

    feat: implement isInside method for Polygon class to determine point containment

--- src/nonview/base/geos/Polygon.js
+++ src/nonview/base/geos/Polygon.js
@@ -11,13 +11,29 @@ export default class Polygon {
 
   static fromRaw(floatPairList) {
     return new Polygon(
-      floatPairList.map((floatPair) => LatLng.fromRaw(floatPair)),
+      floatPairList.map((floatPair) => LatLng.fromRaw(floatPair))
     );
   }
 
   static fromReverseRaw(floatPairList) {
     return new Polygon(
-      floatPairList.map((floatPair) => LatLng.fromReverseRaw(floatPair)),
+      floatPairList.map((floatPair) => LatLng.fromReverseRaw(floatPair))
     );
+  }
+
+  isInside(latLng) {
+    const polygon = this.latLngList.map((l) => l.raw());
+    const [x, y] = latLng.raw();
+
+    let inside = false;
+    for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+      const [xi, yi] = polygon[i];
+      const [xj, yj] = polygon[j];
+
+      const intersect =
+        yi > y !== yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+      if (intersect) inside = !inside;
+    }
+    return inside;
   }
 }
