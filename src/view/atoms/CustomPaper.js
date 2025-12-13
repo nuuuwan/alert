99ee0e19c72305aeccce83ca7729b6441594ae94
nuuuwan <nuuuwan@gmail.com>
2025-12-13 16:39:33 +0530

    feat: update background color and opacity in CustomPaper for improved styling

--- src/view/atoms/CustomPaper.js
+++ src/view/atoms/CustomPaper.js
@@ -6,12 +6,12 @@ export default function CustomPaper({ children, sx }) {
     <Paper
       sx={Object.assign(
         {
-          background: COLORS.neutralLightest,
+          background: "white",
           borderRadius: 2,
           m: 2,
           p: 2,
           width: "fit-content",
-          opacity: 0.8,
+          opacity: 0.9,
         },
         sx
       )}
