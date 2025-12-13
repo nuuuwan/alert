93c8ac639936f6c5715d342e64d118f63c5990be
nuuuwan <nuuuwan@gmail.com>
2025-12-13 12:52:08 +0530

    feat: enhance CustomAppBar with language selection and improve navigation options

--- src/view/atoms/CustomAppBar.js
+++ src/view/atoms/CustomAppBar.js
@@ -4,18 +4,31 @@ import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
+import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import BugReportIcon from "@mui/icons-material/BugReport";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { COLORS } from "../_cons/StyleConstants";
 
 const GITHUB_REPO = "https://github.com/nuuuwan/alert";
 const GITHUB_ISSUES = `${GITHUB_REPO}/issues`;
 
+const LANGUAGE_LABELS = {
+  si: "සිං",
+  ta: "த",
+  en: "En",
+};
+
 export default function CustomAppBar() {
+  const { i18n } = useTranslation();
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+
+  const currentLanguage = i18n.language;
+  const availableLanguages = ["en", "si", "ta"];
 
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -32,6 +45,11 @@ export default function CustomAppBar() {
 
   const handleNavigateToIssues = () => {
     window.open(GITHUB_ISSUES, "_blank");
+    handleMenuClose();
+  };
+
+  const handleLanguageChange = (lang) => {
+    i18n.changeLanguage(lang);
     handleMenuClose();
   };
 
@@ -71,6 +89,31 @@ export default function CustomAppBar() {
             </ListItemIcon>
             Report Issues
           </MenuItem>
+          <Divider />
+          {availableLanguages.map((lang) => (
+            <MenuItem
+              key={lang}
+              onClick={() => handleLanguageChange(lang)}
+              selected={lang === currentLanguage}
+            >
+              <ListItemIcon>
+                <Avatar
+                  sx={{
+                    width: 20,
+                    height: 20,
+                    color: "black",
+                    bgcolor: "transparent",
+                    fontSize: 11,
+                    m: 0,
+                    p: 0,
+                  }}
+                >
+                  {LANGUAGE_LABELS[lang]}
+                </Avatar>
+              </ListItemIcon>
+              {lang.charAt(0).toUpperCase() + lang.slice(1)}
+            </MenuItem>
+          ))}
         </Menu>
       </Toolbar>
     </AppBar>
--- src/view/atoms/CustomBottomNavigator.js
+++ src/view/atoms/CustomBottomNavigator.js
@@ -1,36 +1,18 @@
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Paper from "@mui/material/Paper";
-import Avatar from "@mui/material/Avatar";
 import DownloadIcon from "@mui/icons-material/Download";
 import MyLocationIcon from "@mui/icons-material/MyLocation";
 import PlaceIcon from "@mui/icons-material/Place";
-import { useTranslation } from "react-i18next";
 import { useState } from "react";
 import { COLORS } from "../_cons/StyleConstants";
-
-const LANGUAGE_LABELS = {
-  si: "සිං",
-  ta: "த",
-  en: "En",
-};
 
 export default function CustomBottomNavigator({
   onCurrentLocation,
   onSetToMapCenter,
   onDownload,
 }) {
-  const { i18n } = useTranslation();
   const [value, setValue] = useState(0);
-
-  const currentLanguage = i18n.language;
-  const otherLanguages = ["en", "si", "ta"].filter(
-    (lang) => lang !== currentLanguage
-  );
-
-  const handleLanguageChange = (lang) => {
-    i18n.changeLanguage(lang);
-  };
 
   const handleNavigationChange = (event, newValue) => {
     setValue(newValue);
@@ -41,9 +23,6 @@ export default function CustomBottomNavigator({
       onSetToMapCenter();
     } else if (newValue === 2) {
       onDownload();
-    } else if (newValue >= 3) {
-      const langIndex = newValue - 3;
-      handleLanguageChange(otherLanguages[langIndex]);
     }
   };
 
@@ -68,26 +47,6 @@ export default function CustomBottomNavigator({
         <BottomNavigationAction icon={<MyLocationIcon />} />
         <BottomNavigationAction icon={<PlaceIcon />} />
         <BottomNavigationAction icon={<DownloadIcon />} />
-        {otherLanguages.map((lang, index) => (
-          <BottomNavigationAction
-            key={lang}
-            icon={
-              <Avatar
-                sx={{
-                  width: 24,
-                  height: 24,
-                  color: "black",
-                  bgcolor: "transparent",
-                  fontSize: 12,
-                  m: 0,
-                  p: 0,
-                }}
-              >
-                {LANGUAGE_LABELS[lang]}
-              </Avatar>
-            }
-          />
-        ))}
       </BottomNavigation>
     </Paper>
   );
