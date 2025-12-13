6b9d520acfc3dd1a4080161e5272af380a845a1b
nuuuwan <nuuuwan@gmail.com>
2025-12-13 14:31:35 +0530

    feat: wrap MetricCardCollection in Box for improved layout and styling

--- src/view/moles/NaturalDisasterView.js
+++ src/view/moles/NaturalDisasterView.js
@@ -11,6 +11,7 @@ import NaturalDisaster from "../../nonview/core/third_party/NaturalDisaster";
 import OpenElevation from "../../nonview/core/third_party/OpenElevation";
 import OpenMeteo from "../../nonview/core/third_party/OpenMeteo";
 import { useTranslation } from "react-i18next";
+import Box from "@mui/material/Box";
 
 export default function NaturalDisasterView({ place }) {
   const { openMeteoData, openElevationData, earthquakeData } = place;
@@ -29,96 +30,98 @@ export default function NaturalDisasterView({ place }) {
   });
 
   return (
-    <MetricCardCollection
-      title="Probabilistic Disaster Risk Alerts (Experimental)"
-      sourceList={[
-        ...OpenElevation.getSourceList(),
-        ...OpenMeteo.getSourceList(place.latLng),
-      ]}
-    >
-      <Alert severity="warning" sx={{ mb: 1 }}>
+    <Box>
+      <MetricCardCollection
+        title="Probabilistic Disaster Risk Alerts (Experimental)"
+        sourceList={[
+          ...OpenElevation.getSourceList(),
+          ...OpenMeteo.getSourceList(place.latLng),
+        ]}
+      >
+        <MetricCard
+          Icon={FloodIcon}
+          label="Flood"
+          value={`${floodRiskData.floodRiskLevel}/${floodRiskData.floodRiskMaxLevel}`}
+          unit=""
+          timeLabel="Next 24h"
+          alertLabel={NaturalDisaster.getLabel(
+            floodRiskData.floodRiskLevel,
+            floodRiskData.floodRiskMaxLevel
+          )}
+          color={getAlertColor(
+            floodRiskData.floodRiskLevel,
+            floodRiskData.floodRiskMaxLevel
+          )}
+        />
+        <MetricCard
+          Icon={LandslideIcon}
+          label="Landslide"
+          value={`${landslideRiskData.landslideRiskLevel}/${landslideRiskData.landslideRiskMaxLevel}`}
+          unit=""
+          timeLabel="Next 24h"
+          alertLabel={NaturalDisaster.getLabel(
+            landslideRiskData.landslideRiskLevel,
+            landslideRiskData.landslideRiskMaxLevel
+          )}
+          color={getAlertColor(
+            landslideRiskData.landslideRiskLevel,
+            landslideRiskData.landslideRiskMaxLevel
+          )}
+        />
+
+        <MetricCard
+          Icon={DeviceThermostatIcon}
+          label="Heat"
+          value={`${heatRiskData.heatRiskLevel}/${heatRiskData.heatRiskMaxLevel}`}
+          unit=""
+          timeLabel="Next 24h"
+          alertLabel={NaturalDisaster.getLabel(
+            heatRiskData.heatRiskLevel,
+            heatRiskData.heatRiskMaxLevel
+          )}
+          color={getAlertColor(
+            heatRiskData.heatRiskLevel,
+            heatRiskData.heatRiskMaxLevel
+          )}
+        />
+
+        <MetricCard
+          Icon={InvertColorsOffIcon}
+          label="Drought"
+          value={`${droughtRiskData.droughtRiskLevel}/${droughtRiskData.droughtRiskMaxLevel}`}
+          unit=""
+          timeLabel="Next 24h"
+          alertLabel={NaturalDisaster.getLabel(
+            droughtRiskData.droughtRiskLevel,
+            droughtRiskData.droughtRiskMaxLevel
+          )}
+          color={getAlertColor(
+            droughtRiskData.droughtRiskLevel,
+            droughtRiskData.droughtRiskMaxLevel
+          )}
+        />
+
+        <MetricCard
+          Icon={TsunamiIcon}
+          label="Tsunami"
+          value={`${tsunamiRiskData.tsunamiRiskLevel}/${tsunamiRiskData.tsunamiRiskMaxLevel}`}
+          unit=""
+          timeLabel="Next 24h"
+          alertLabel={NaturalDisaster.getLabel(
+            tsunamiRiskData.tsunamiRiskLevel,
+            tsunamiRiskData.tsunamiRiskMaxLevel
+          )}
+          color={getAlertColor(
+            tsunamiRiskData.tsunamiRiskLevel,
+            tsunamiRiskData.tsunamiRiskMaxLevel
+          )}
+        />
+      </MetricCardCollection>
+      <Alert severity="warning" sx={{ ml: 1, width: "fit-content" }}>
         {t(
-          "These Natural Disaster Risk Metrics are still under development and should be used for informational purposes only.",
+          "These Natural Disaster Risk Metrics are still under development and should be used for informational purposes only."
         )}
       </Alert>
-      <MetricCard
-        Icon={FloodIcon}
-        label="Flood"
-        value={`${floodRiskData.floodRiskLevel}/${floodRiskData.floodRiskMaxLevel}`}
-        unit=""
-        timeLabel="Next 24h"
-        alertLabel={NaturalDisaster.getLabel(
-          floodRiskData.floodRiskLevel,
-          floodRiskData.floodRiskMaxLevel,
-        )}
-        color={getAlertColor(
-          floodRiskData.floodRiskLevel,
-          floodRiskData.floodRiskMaxLevel,
-        )}
-      />
-      <MetricCard
-        Icon={LandslideIcon}
-        label="Landslide"
-        value={`${landslideRiskData.landslideRiskLevel}/${landslideRiskData.landslideRiskMaxLevel}`}
-        unit=""
-        timeLabel="Next 24h"
-        alertLabel={NaturalDisaster.getLabel(
-          landslideRiskData.landslideRiskLevel,
-          landslideRiskData.landslideRiskMaxLevel,
-        )}
-        color={getAlertColor(
-          landslideRiskData.landslideRiskLevel,
-          landslideRiskData.landslideRiskMaxLevel,
-        )}
-      />
-
-      <MetricCard
-        Icon={DeviceThermostatIcon}
-        label="Heat"
-        value={`${heatRiskData.heatRiskLevel}/${heatRiskData.heatRiskMaxLevel}`}
-        unit=""
-        timeLabel="Next 24h"
-        alertLabel={NaturalDisaster.getLabel(
-          heatRiskData.heatRiskLevel,
-          heatRiskData.heatRiskMaxLevel,
-        )}
-        color={getAlertColor(
-          heatRiskData.heatRiskLevel,
-          heatRiskData.heatRiskMaxLevel,
-        )}
-      />
-
-      <MetricCard
-        Icon={InvertColorsOffIcon}
-        label="Drought"
-        value={`${droughtRiskData.droughtRiskLevel}/${droughtRiskData.droughtRiskMaxLevel}`}
-        unit=""
-        timeLabel="Next 24h"
-        alertLabel={NaturalDisaster.getLabel(
-          droughtRiskData.droughtRiskLevel,
-          droughtRiskData.droughtRiskMaxLevel,
-        )}
-        color={getAlertColor(
-          droughtRiskData.droughtRiskLevel,
-          droughtRiskData.droughtRiskMaxLevel,
-        )}
-      />
-
-      <MetricCard
-        Icon={TsunamiIcon}
-        label="Tsunami"
-        value={`${tsunamiRiskData.tsunamiRiskLevel}/${tsunamiRiskData.tsunamiRiskMaxLevel}`}
-        unit=""
-        timeLabel="Next 24h"
-        alertLabel={NaturalDisaster.getLabel(
-          tsunamiRiskData.tsunamiRiskLevel,
-          tsunamiRiskData.tsunamiRiskMaxLevel,
-        )}
-        color={getAlertColor(
-          tsunamiRiskData.tsunamiRiskLevel,
-          tsunamiRiskData.tsunamiRiskMaxLevel,
-        )}
-      />
-    </MetricCardCollection>
+    </Box>
   );
 }
