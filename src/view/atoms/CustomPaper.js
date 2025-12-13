d61b6090a7e3aecaf8512377be3c23ae4fa9a0a6
nuuuwan <nuuuwan@gmail.com>
2025-12-13 17:35:44 +0530

    feat: ensure elevation prop is consistently applied in CustomPaper component

--- src/view/atoms/CustomPaper.js
+++ src/view/atoms/CustomPaper.js
@@ -3,6 +3,7 @@ import Paper from "@mui/material/Paper";
 export default function CustomPaper({ children, sx }) {
   return (
     <Paper
+      elevation={10}
       sx={Object.assign(
         {
           background: "white",
@@ -12,7 +13,7 @@ export default function CustomPaper({ children, sx }) {
           width: "fit-content",
           opacity: 0.9,
         },
-        sx,
+        sx
       )}
     >
       {children}
