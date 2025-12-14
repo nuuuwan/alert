67ec0f62cf724f2959a71744e505e5c9523ac935
nuuuwan <nuuuwan@gmail.com>
2025-12-14 17:36:03 +0530

    fix: add customizable icon prop to InformationGroup for enhanced flexibility

--- src/view/atoms/InformationGroup.js
+++ src/view/atoms/InformationGroup.js
@@ -4,12 +4,19 @@ import Typography from "@mui/material/Typography";
 import DataSourceView from "./DataSourceView";
 import { useTranslation } from "react-i18next";
 import CustomPaper from "./CustomPaper";
+import InfoIcon from "@mui/icons-material/Info";
 
-export default function InformationGroup({ title, children, dataSourceList }) {
+export default function InformationGroup({
+  title,
+  children,
+  dataSourceList,
+  icon: Icon = InfoIcon,
+}) {
   const { t } = useTranslation();
   return (
     <CustomPaper>
-      <Box sx={{ mb: 1 }}>
+      <Box sx={{ mb: 1, display: "flex", alignItems: "center", gap: 1 }}>
+        <Icon />
         <Typography variant="h6" fontWeight={600}>
           {t(title)}
         </Typography>
