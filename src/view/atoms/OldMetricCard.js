9a67a6d525843d0876651efe7158af3a5675a954
nuuuwan <nuuuwan@gmail.com>
2025-12-14 11:04:39 +0530

    feat: update header text in OldMetricCard component to "OLD"

--- src/view/atoms/OldMetricCard.js
+++ src/view/atoms/OldMetricCard.js
@@ -39,7 +39,7 @@ export default function OldMetricCard({
         overflow: "hidden", // Ensure content does not overflow
       }}
     >
-      <Typography variant="body1">OLD METRIC CARD</Typography>
+      <Typography variant="body1">OLD</Typography>
 
       <Box
         sx={{
