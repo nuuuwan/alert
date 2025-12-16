7225b270ee757afae12a6f95efc1ab8454a1f891
nuuuwan <nuuuwan@gmail.com>
2025-12-16 19:05:13 +0530

    fix: correct water level alert level reference and update data source labels in NaturalDisasterOfficialView

--- src/view/moles/NaturalDisasterOfficialView.js
+++ src/view/moles/NaturalDisasterOfficialView.js
@@ -24,7 +24,7 @@ export default function NaturalDisasterOfficialView({ place }) {
       new DataSource({
         label: "Disaster Management Centre of Sri Lanka",
         url: "https://www.dmc.gov.lk",
-      }),
+      })
     );
   }
 
@@ -34,7 +34,7 @@ export default function NaturalDisasterOfficialView({ place }) {
         label:
           "Hydrology and Disaster Management Division, Irrigation Deptartment of Sri Lanka",
         url: "https://github.com/nuuuwan/lk_irrigation",
-      }),
+      })
     );
   }
 
@@ -83,7 +83,7 @@ function getLandslideCard(dsd) {
 }
 
 function getWaterLevelCard(hydrometricStation) {
-  const level = hydrometricStation.alertLevel;
+  const level = hydrometricStation.waterLevelAlertLevel;
   const waterLevelM = hydrometricStation.latestWaterLevelM;
   const timeUt = hydrometricStation.latestWaterLevelTimeUt;
 
