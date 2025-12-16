677d9aee804354509c2a8c99bcd2bb576e9b88b7
nuuuwan <nuuuwan@gmail.com>
2025-12-16 14:07:18 +0530

    fix: update maxWidth in Alert component for better responsiveness

--- src/view/moles/NaturalDisasterView.js
+++ src/view/moles/NaturalDisasterView.js
@@ -41,7 +41,7 @@ export default function NaturalDisasterView({ place }) {
       <Alert
         severity="warning"
         icon={<ScienceIcon />}
-        sx={{ ml: 3, width: "fit-content", maxWidth: 320 }}
+        sx={{ ml: 3, width: "fit-content", maxWidth: "90vw" }}
       >
         {t(
           "The following Natural Disaster Risk Scores are automtically generated, and are still under development and should be used for informational purposes only."
