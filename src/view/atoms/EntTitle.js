fa5fb04df1a9d719faebb329f07ca61ef9d764e9
nuuuwan <nuuuwan@gmail.com>
2025-12-21 16:20:47 +0530

    refactor: integrate translation for entity title in EntTitle component

--- src/view/atoms/EntTitle.js
+++ src/view/atoms/EntTitle.js
@@ -2,9 +2,11 @@ import { Box, Stack, Typography } from "@mui/material";
 import EntAvatar from "./EntAvatar";
 import Place from "../../nonview/core/ents/places/Place";
 import DSDLocationBreadcrumbs from "../moles/DSDLocationBreadcrumbs";
+import { useTranslation } from "react-i18next";
 
 export default function EntTitle({ ent, mapLatLng }) {
   const entDisplay = ent || Place.fromLatLng(mapLatLng);
+  const { t } = useTranslation();
 
   document.title = entDisplay.title;
   return (
@@ -12,7 +14,7 @@ export default function EntTitle({ ent, mapLatLng }) {
       <Stack direction="row" alignItems="center" spacing={1}>
         <EntAvatar ent={entDisplay} color="white" size="32px" />
         <Stack direction="column" spacing={0} justifyContent="center">
-          <Typography variant="body1">{entDisplay.title}</Typography>
+          <Typography variant="body1">{t(entDisplay.title)}</Typography>
           <DSDLocationBreadcrumbs ent={entDisplay} />
         </Stack>
       </Stack>
