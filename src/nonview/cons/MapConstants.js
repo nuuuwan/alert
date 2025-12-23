019300abe144af77e9a00cffc4e76e33e0da514a
nuuuwan <nuuuwan@gmail.com>
2025-12-23 14:02:02 +0530

    feat: add MIN_LAT_LNG and MAX_LAT_LNG constants for geographical boundaries

--- src/nonview/cons/MapConstants.js
+++ src/nonview/cons/MapConstants.js
@@ -2,6 +2,10 @@ export const MIN_LAT = 5.9186;
 export const MAX_LAT = 9.8352;
 export const MIN_LNG = 79.6954;
 export const MAX_LNG = 81.8815;
+
+export const MIN_LAT_LNG = [MIN_LAT, MIN_LNG];
+export const MAX_LAT_LNG = [MAX_LAT, MAX_LNG];
+
 export const DEFAULT_CENTER = [
   (MIN_LAT + MAX_LAT) / 2,
   (MIN_LNG + MAX_LNG) / 2,
