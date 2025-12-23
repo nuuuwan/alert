3b2442d2df8cc4a3d32701c2adb29ed460c5d9de
nuuuwan <nuuuwan@gmail.com>
2025-12-23 14:01:34 +0530

    feat: update DEFAULT_CENTER calculation using MIN and MAX latitude/longitude constants

--- src/nonview/cons/MapConstants.js
+++ src/nonview/cons/MapConstants.js
@@ -1,4 +1,11 @@
-export const DEFAULT_CENTER = [7.8731, 80.7718];
+export const MIN_LAT = 5.9186;
+export const MAX_LAT = 9.8352;
+export const MIN_LNG = 79.6954;
+export const MAX_LNG = 81.8815;
+export const DEFAULT_CENTER = [
+  (MIN_LAT + MAX_LAT) / 2,
+  (MIN_LNG + MAX_LNG) / 2,
+];
 export const DEFAULT_ZOOM = 8;
 
 const MapConstants = {
