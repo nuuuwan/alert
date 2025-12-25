f8cead69abbbce18fbaee3decd6ca1ab67ee946a
nuuuwan <nuuuwan@gmail.com>
2025-12-25 10:54:26 +0530

    refactor: simplify tab configuration in NaturalDisasterOfficialView

--- src/view/moles/NaturalDisasterOfficialView.js
+++ src/view/moles/NaturalDisasterOfficialView.js
@@ -10,32 +10,36 @@ export default function NaturalDisasterOfficialView({ place }) {
     return <CircularProgress />;
   }
 
+  const isHydrometricStation = place instanceof HydrometricStation;
+
+  let tabToChild = {
+    Landslides: () => <LandslideCard dsd={place.dsd} />,
+  };
+  let tabToColor = {
+    Landslides: getAlertColor(place.dsd.latestLandslideWarningLevel, 3),
+  };
+  let tabToNAlerts = {
+    Landslides:
+      place.dsd.latestLandslideWarningLevel &&
+      place.dsd.latestLandslideWarningLevel > 0
+        ? 1
+        : 0,
+  };
+
+  if (isHydrometricStation) {
+    tabToChild["Water Level"] = () => (
+      <WaterLevelCard hydrometricStation={place} />
+    );
+    tabToColor["Water Level"] = getAlertColor(place.waterLevelAlertLevel, 3);
+    tabToNAlerts["Water Level"] =
+      place.waterLevelAlertLevel && place.waterLevelAlertLevel > 0 ? 1 : 0;
+  }
+
   return (
     <CustomTabs
-      tabToChild={{
-        Landslides: () => <LandslideCard dsd={place.dsd} />,
-        "Water Level": () => <WaterLevelCard hydrometricStation={place} />,
-      }}
-      tabToColor={{
-        Landslides: getAlertColor(place.dsd.latestLandslideWarningLevel, 3),
-        "Water Level":
-          place instanceof HydrometricStation
-            ? getAlertColor(place.waterLevelAlertLevel, 3)
-            : undefined,
-      }}
-      tabToNAlerts={{
-        Landslides:
-          place.dsd.latestLandslideWarningLevel &&
-          place.dsd.latestLandslideWarningLevel > 0
-            ? 1
-            : 0,
-        "Water Level":
-          place instanceof HydrometricStation &&
-          place.waterLevelAlertLevel &&
-          place.waterLevelAlertLevel > 0
-            ? 1
-            : 0,
-      }}
+      tabToChild={tabToChild}
+      tabToColor={tabToColor}
+      tabToNAlerts={tabToNAlerts}
     />
   );
 }
