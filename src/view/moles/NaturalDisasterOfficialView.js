34a795e425bd962d82c941190b9fd1ebd550b317
nuuuwan <nuuuwan@gmail.com>
2025-12-25 10:45:18 +0530

    refactor: remove unused data source handling from NaturalDisasterOfficialView

--- src/view/moles/NaturalDisasterOfficialView.js
+++ src/view/moles/NaturalDisasterOfficialView.js
@@ -1,8 +1,6 @@
 import { getAlertColor } from "../_cons/StyleConstants";
 import HydrometricStation from "../../nonview/core/ents/places/HydrometricStation";
-import DataSource from "../../nonview/core/DataSource";
 import { CircularProgress } from "@mui/material";
-import DataSourceView from "../atoms/DataSourceView";
 import CustomTabs from "../atoms/CustomTabs";
 import LandslideCard from "./LandslideCard";
 import WaterLevelCard from "./WaterLevelCard";
@@ -10,33 +8,6 @@ import WaterLevelCard from "./WaterLevelCard";
 export default function NaturalDisasterOfficialView({ place }) {
   if (!place) {
     return <CircularProgress />;
-  }
-
-  const hasLandslide = place.dsd.latestLandslideWarningLevel !== null;
-  const hasWaterLevel = place instanceof HydrometricStation;
-
-  let dataSourceList = [];
-  if (hasLandslide) {
-    dataSourceList.push(
-      new DataSource({
-        label: "Disaster Management Centre of Sri Lanka",
-        url: "https://www.dmc.gov.lk",
-      }),
-    );
-  }
-
-  if (hasWaterLevel) {
-    dataSourceList.push(
-      new DataSource({
-        label:
-          "Hydrology and Disaster Management Division, Irrigation Deptartment of Sri Lanka",
-        url: "https://github.com/nuuuwan/lk_irrigation",
-      }),
-    );
-  }
-
-  if (!hasLandslide && !hasWaterLevel) {
-    return <DataSourceView dataSourceList={dataSourceList} />;
   }
 
   return (
